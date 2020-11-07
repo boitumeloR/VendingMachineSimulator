ab0ed3020d5aa1ea5313e2c4c3112a3890c97aa6
Boitumelo Rampete <u17120323@tuks.co.za>
2020-11-07 20:09:11 +0200

    Output coin when clicked

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Coin, DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-coins',
   templateUrl: './coins.component.html',
   styleUrls: ['./coins.component.scss']
 })
-export class CoinsComponent implements OnInit {
+export class CoinsComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  mockData: Coin[] = [
+    { CoinID: 1, CoinQuantity: 2, CoinValue: 0.5},
+    { CoinID: 1, CoinQuantity: 3, CoinValue: 1},
+    { CoinID: 1, CoinQuantity: 1, CoinValue: 2},
+    { CoinID: 1, CoinQuantity: 4, CoinValue: 5}
+  ];
+  coinData$: Observable<Coin[]>;
+  coinData: Coin[];
+
+  // @Input() bought: number;
+  @Output() coinClicked = new EventEmitter<Coin>();
+  constructor(private dataServ: DataService) { }
 
   ngOnInit(): void {
+    // this.refreshCoins();
+  }
+
+  ngOnChanges() {
+    // Once input property changes, we refresh the coin information
+  }
+
+  refreshCoins() {
+    // Get coin information from api
+    this.coinData$ = this.dataServ.GetAllCoins();
+    this.coinData$.subscribe(data => {
+      this.coinData = data;
+    });
+  }
+
+  enterCoin(enteredCoin: Coin): void {
+    this.coinClicked.emit(enteredCoin);
   }
 
 }
