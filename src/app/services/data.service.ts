10c9ba1ab70733ce79682e9d134157d41d097d92
Boitumelo Rampete <u17120323@tuks.co.za>
2020-11-07 19:31:18 +0200

    Service api calls & data interfaces

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Coin {
+  CoinID: number;
+  CoinValue: number;
+  CoinQuantity: number;
+}
+
+export interface Product {
+  ProductID: number;
+  ProductName: string;
+  ProductPrice: number;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +21,11 @@ export class DataService {
   serverDomain = 'https://localhost:44371';
   constructor(private http: HttpClient) { }
 
-  GetAllCoins() {
+  GetAllCoins(): Observable<Coin[]> {
+    return this.http.get<Coin[]>(`${this.serverDomain}/api/Vending/GetCoins`);
+  }
 
+  GetProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.serverDomain}/api/Vending/GetCoins`);
   }
 }
